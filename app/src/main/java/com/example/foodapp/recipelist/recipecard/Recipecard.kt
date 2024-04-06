31554c37e753080a3e221d5ba265bb44d4d076df
Furatus <furacoptere@gmail.com>
2024-04-06 16:41:18 +0200

    test name ?

--- app/src/main/java/com/example/foodapp/recipelist/recipecard/Recipecard.kt
+++ app/src/main/java/com/example/foodapp/recipelist/recipecard/Recipecard.kt
@@ -1,6 +1,7 @@
 package com.example.foodapp.recipelist.recipecard
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,14 +23,20 @@ import coil.request.ImageRequest
 fun RecipeCard(ImageUrl : String, RecipeName : String) {
 OutlinedCard (modifier = Modifier
     .fillMaxWidth()
-    .height(350.dp).requiredHeight(350.dp).padding(16.dp)){
+    .height(350.dp)
+    .requiredHeight(350.dp)
+    .padding(16.dp)){
     Column (modifier = Modifier.padding(16.dp)) {
         val imageloader = ImageLoader(context = LocalContext.current)
         AsyncImage(model = ImageRequest.Builder(LocalContext.current).data(ImageUrl).crossfade(true).build(), contentDescription = "description_test", imageLoader = imageloader, modifier = Modifier
             .height(150.dp)
             .fillMaxWidth())
         Spacer(modifier = Modifier.padding(16.dp))
-        Text(text = RecipeName, Modifier.size(50.dp).fillMaxWidth())
+
+        Row {
+            Text(text = RecipeName/*, Modifier.size(50.dp).fillMaxWidth()*/)
+            Text(text = "test");
+        }
     }
 }
 }
